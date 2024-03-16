8207dea09c07349ad9bd7897f297205df54ffab4
JosefinRobertsson <josefinrobertsson@hotmail.com>
2024-03-16 13:45:52 +0100

    added .memo to prevent rerender on every state update

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,40 +5,42 @@ import { ProgressContext } from './ProgressProvider';
 const ProgressBar = ({ previousStep }) => {
     const { progress, prevProgress } = React.useContext(ProgressContext);
     const [isIncreasing, setIsIncreasing] = React.useState(false);
+    const [from, setFrom] = React.useState(0);
+    const [to, setTo] = React.useState(0);
+    const [latestStep, setLatestStep] = React.useState(0);
 
     React.useEffect(() => {
         if (progress < prevProgress) {
             setIsIncreasing(false);
         } else {
-        setIsIncreasing(true);
+            setIsIncreasing(true);
         }
+        setLatestStep(progress);
+        console.log('isIncreasing updated:');
     }, [progress, prevProgress]);
 
 
-    const calculateDashoffset = (directionValue) => {
-        const totalLength = 274; // Total length of the stroke-dasharray
-        if (isIncreasing) {
-            return totalLength * (1 - directionValue / 7);
-        } else {
-            return totalLength * (1 - (directionValue + 1) / 7);
-        }
-    };
-      
-    // Call calculateDashoffset with the correct values depending on whether the progress is increasing or decreasing
-    const from = calculateDashoffset(isIncreasing ? previousStep : progress);
+    // Define calculateDashoffset inside useEffect to ensure it gets the latest isIncreasing value
+    React.useEffect(() => {
+        const calculateDashoffset = (directionValue) => {
+            const totalLength = 274; // Total length of the stroke-dasharray
+            console.log('directionValue:', directionValue);
+            if (isIncreasing) { 
+                return totalLength * (1 - directionValue / 7);
+            } else {
+                return totalLength * (1 - (directionValue + 1) / 7);
+            }
+        };
+
+
+         // Update state with new values
+         setFrom(_prevFrom => calculateDashoffset(isIncreasing ? previousStep : latestStep));
+         setTo(_prevTo => calculateDashoffset(isIncreasing ? latestStep : previousStep));
+         
+    }, [isIncreasing, previousStep, latestStep]);
     console.log('from:', from);
-    
-    const to = calculateDashoffset(isIncreasing ? progress : previousStep);
     console.log('to:', to);
     
-
-
-    /*
-    const from = isIncreasing ? calculateDashoffset(previousStep) :  calculateDashoffset(progress);
-    
-    const to = isIncreasing ? calculateDashoffset(progress) : calculateDashoffset(previousStep);
-    */
-
     return (
         <div className="progress-wrapper">
             <div className="progress-container">
@@ -46,15 +48,18 @@ const ProgressBar = ({ previousStep }) => {
                     <div className="progress-outer">
                         <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
                             <defs>
-                                <linearGradient id="gradient-color">
-                                    <stop offset="0%" stopColor="#EE82EE" />
-                                    <stop offset="100%" stopColor="#800080" />
+                                <linearGradient id="gradient-color"
+                                x1="0%" y1="0%" x2="0%" y2="100%">
+                                    <stop offset="20%" stopColor="#EE82EE" />
+                                    <stop offset="90%" stopColor="#800080" 
+                                    />
                                 </linearGradient>
                             </defs>
                             <circle
                                 cx="49%"
                                 cy="49%"
                                 r="44%"
+                                transform="rotate(-90 50 50)"
                                 fill="none"
                                 stroke="url(#gradient-color)"
                                 strokeWidth="10px"
@@ -66,7 +71,7 @@ const ProgressBar = ({ previousStep }) => {
                                     attributeName="stroke-dashoffset"
                                     from={from} // Set from value accordingly
                                     to={to} // Set to value accordingly
-                                    dur="1s"
+                                    dur="0.5s"
                                     fill="freeze"
                                 />
                             </circle>
@@ -84,4 +89,4 @@ const ProgressBar = ({ previousStep }) => {
     );
 }
 
-export default ProgressBar;
+export default React.memo(ProgressBar);
