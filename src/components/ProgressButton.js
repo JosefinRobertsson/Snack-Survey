4031ee15e4d98dc9751eb86b1d4b81206f6b6a07
JosefinRobertsson <josefinrobertsson@hotmail.com>
2024-03-23 21:41:57 +0100

    added sound effect to nextbutton

--- src/components/ProgressButton.js
+++ src/components/ProgressButton.js
@@ -6,6 +6,7 @@ import { PriceContext } from './PriceProvider';
 import { SnackSuggestionContext } from './SnackSuggestionProvider';
 import { resetAnswers } from '../helpers/reset-state.helpers';
 import { BaseButton } from '../styles/buttons.styles';
+import nextSoundEffect from '../sounds/next-button-effect.wav';
 
 const ProgressButton = ({ hideButton, currentAnswer, questionID, progressButtonName, backButtonName }) => {
     const { progress, setProgress, setPrevProgress, setShowProgressBar } = React.useContext(ProgressContext);
@@ -23,14 +24,23 @@ const ProgressButton = ({ hideButton, currentAnswer, questionID, progressButtonN
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const playSound = () => {
+        const nextButtonAudio = new Audio(nextSoundEffect);
+        nextButtonAudio.volume = 0.5;
+        nextButtonAudio.play();
+      }
+
+
     const handleProgressClick = (id) => {
         if (id === "nextBtn") {
             // reset the state of all coming questions if the user changes their answer
             if ((typeof prevAnswerRef.current != "undefined" && prevAnswerRef.current !== currentAnswer) || (questionID === "0")) {
                 resetAnswers(questionID, setType, setTaste, setTexture, setFlavor, setPrice, setImportantCategories, setSuggestion, setDropdown, setShowProgressBar);
             }
-            setProgress(progress + 1);
-            setPrevProgress(progress);
+            playSound();
+            setTimeout(() => 
+            setProgress(progress + 1), 500)
+            setPrevProgress(progress)
         } else if (id === "backBtn") {
             setPrevProgress(progress);
             setProgress(progress - 1);
