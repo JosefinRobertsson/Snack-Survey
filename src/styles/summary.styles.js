a4279ff6c39d3d245bc7ba362de3bb58a55648a6
JosefinRobertsson <josefinrobertsson@hotmail.com>
2024-03-24 17:21:15 +0100

    responsiveness changes

--- src/styles/summary.styles.js
+++ src/styles/summary.styles.js
@@ -16,5 +16,12 @@ export const ResultSpan = styled.span`
   font-family: 'Permanent Marker', cursive;
   color: magenta;
   opacity: 0.7; 
+  padding-right: 6px;
   animation: ${glow} 1.5s infinite alternate;
+
+  @media screen and (max-width: 667px) {
+    animation: none; 
+    text-shadow: 2px 2px 3px rgba(78, 163, 101, 0.7);
+  }
 `;
+
