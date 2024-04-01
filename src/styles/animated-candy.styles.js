2364276f891f0a054b871f18ac7106b42606a1e7
JosefinRobertsson <josefinrobertsson@hotmail.com>
2024-04-01 18:34:09 +0200

    slower animation gummy bears

--- src/styles/animated-candy.styles.js
+++ src/styles/animated-candy.styles.js
@@ -2,7 +2,7 @@ import styled, { keyframes } from "styled-components";
 
 const driftDown = keyframes`
   0% {
-    transform: translateY(-100%) translateX(-12%);
+    transform: translateY(-100%) translateX(-25%);
   }
   5% {
     transform: translateY(5vh) translateX(25%);
@@ -69,6 +69,76 @@ const driftDown = keyframes`
   }
 `;
 
+const slowDriftDown = keyframes`
+  0% {
+    transform: translateY(0vh) translateX(-25%) rotate(-5deg);
+  }
+  5% {
+    transform: translateY(5vh) translateX(0%) rotate(0deg);
+  }
+  10% {
+    transform: translateY(10vh) translateX(25%) rotate(5deg);
+  }
+  15% {
+    transform: translateY(15vh) translateX(0%) rotate(0deg);
+  }
+  20% {
+    transform: translateY(20vh) translateX(-25%) rotate(-5deg);
+  }
+  25% {
+    transform: translateY(25vh) translateX(0%) rotate(0deg);
+  }
+  30% {
+    transform: translateY(30vh) translateX(25%) rotate(5deg);
+  }
+  35% {
+    transform: translateY(35vh) translateX(0%) rotate(0deg);
+  }
+  40% {
+    transform: translateY(40vh) translateX(-25%) rotate(-5deg);
+  }
+  45% {
+    transform: translateY(45vh) translateX(0%) rotate(0deg);
+  }
+  50% {
+    transform: translateY(50vh) translateX(25%) rotate(5deg);
+  }
+  55% {
+    transform: translateY(55vh) translateX(0%) rotate(0deg);
+  }
+  60% {
+    transform: translateY(60vh) translateX(-25%) rotate(-5deg);
+  }
+  65% {
+    transform: translateY(65vh) translateX(0%) rotate(0deg);
+  }
+  70% {
+    transform: translateY(70vh) translateX(25%) rotate(5deg);
+  }
+  75% {
+    transform: translateY(75vh) translateX(0%) rotate(0deg);
+  }
+  80% {
+    transform: translateY(80vh) translateX(-25%) rotate(-5deg);
+  }
+  85% {
+    transform: translateY(85vh) translateX(0%) rotate(0deg);
+  }
+  90% {
+    transform: translateY(90vh) translateX(25%) rotate(5deg);
+  }
+  95% {
+    transform: translateY(95vh) translateX(0%) rotate(0deg);
+  }
+  99.99% {
+    transform: translateY(100vh) translateX(0%) rotate(-5deg);
+  }
+  100% {
+    transform: translateY(100vh) translateX(-25%) rotate(-5deg);
+  }
+`;
+
+
 
 export const AnimatedCandyContainer = styled.div`
     position: fixed;
@@ -78,7 +148,7 @@ export const AnimatedCandyContainer = styled.div`
     z-index: 10;
     width: 50px;
     height: 50px;
-    filter: drop-shadow(0 0 6px rgba(0, 0, 0, 0.5));
+    filter: drop-shadow(0 0 6px rgba(0, 0, 0, 0.5)) brightness(115%);
     animation: ${driftDown} 15s linear infinite;
     backface-visibility: hidden;
 
@@ -92,7 +162,7 @@ export const AnimatedCandyContainer = styled.div`
 
 export const AnimatedCandyContainer2 = styled(AnimatedCandyContainer)`
 left: 45%;
-animation: ${driftDown} 19s linear infinite;
+animation: ${slowDriftDown} 19s linear infinite;
 
 @media screen and (max-width: 2300px) {
         left: 40%;
